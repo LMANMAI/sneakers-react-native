05c89c68c0263e8cf4fbdaabe17d688a0fef4c5c
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2024-03-21 22:14:30 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const HomeScreen = () => {
+  return <View>Home</View>;
+};
+
+export default HomeScreen;
---
+++ src/screens/index.ts
@@ -0,0 +1,1 @@
+export {default as HomeScrenn} from './Home';
